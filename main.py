107cbbef64e6c0713df60086a70b4eb9e32493b0
Vairann <Kachurat2006@gmail.com>
2025-05-08 03:08:15 +0500

    New update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 for i in range (1 , 21):
     if (i **0.5).is_integer():
         print(i)
+for i in range (1 , 21):
+    if (i **0.5).is_integer():
+        print(i) 
+b = dict()
+stroka = "hello!"
+stroka=stroka.lower().strip()
+for char in stroka:
+    b[char] = b.get(char , 0) +1
+print (b)    
