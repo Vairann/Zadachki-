f727c1be0a69d071db4c18f2e7bbb9d531e27b2c
Vairann <Kachurat2006@gmail.com>
2025-05-28 22:25:58 +0500

    Начало pytest

--- ka.py
+++
@@ -1,8 +0,0 @@
-vvod =(input().strip())
-try:
-    result = eval(vvod)
-    print (f"Результат : {result}")
-except ZeroDivisionError:
-    print ("Ошибка делеления на ноль")
-except NameError:
-    print("Неверный ввод")        
